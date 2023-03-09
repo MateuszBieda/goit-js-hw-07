328a1b872e213b993d7a9a842fafa8aafacec4e5
MateuszBieda <mateusbieda@gmail.com>
2023-03-10 00:17:32 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const list = galleryItems.map((image)=> 
+`<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+</div>`).join(" ");
+
+//console.log(list);
+gallery.insertAdjacentHTML('beforeend', list);
